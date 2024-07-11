07db82d331bca7333bfff2178e4b7d2ab0c2d3e1
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-07-11 12:08:51 +0900

    Update forklift(merge).py

--- forklift(merge).py
+++ forklift(merge).py
@@ -4,15 +4,12 @@ import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 
 # Streamlit 페이지 설정
-st.set_page_config(page_title='My Streamlit App', layout='wide', initial_sidebar_state='collapsed')
+st.set_page_config(page_title='My Streamlit App', layout='wide', initial_sidebar_state='expanded')
 
-# 레이아웃을 위한 컬럼 설정
-col1, col2 = st.columns([1, 3])
-
-with col1:
+# Streamlit 사이드바 설정
+with st.sidebar:
     # Streamlit 파일 업로더를 사용하여 파일 업로드
     uploaded_file = st.file_uploader("파일을 업로드하세요.", type=["csv"])
-
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
 
@@ -24,22 +21,8 @@ with col1:
         # 필터링 및 전처리 옵션
         analysis_type = st.radio(
             "분석 유형 선택:",
-            ('운영 대수', '운영 횟수'),
-            horizontal=True
+            ('운영 대수', '운영 횟수')
         )
-
-        if analysis_type == '운영 대수':
-            df['시작 날짜'] = pd.to_datetime(df['시작 날짜']).dt.strftime('%d')
-            df = df.sort_values(by=['시작 날짜', '시간대'])
-            index_name = '시작 날짜'
-            value_name = '차대 코드'
-            agg_func = 'nunique'
-            title = '시작 날짜 및 시간대별 차대 코드 운영 대수 Heatmap'
-        else:
-            index_name = '차대 코드'
-            value_name = '시작 날짜'
-            agg_func = 'count'
-            title = '차대 코드별 시간대 운영 횟수 Heatmap'
 
         # 선택 상자를 통한 부서와 차대 분류 선택
         selected_department = st.selectbox(
@@ -52,44 +35,55 @@ with col1:
         )
 
         # 그래프 높이 조정 슬라이더
-        graph_height = st.slider('Select graph height', 300, 1500, 900, key='height_slider')
+        graph_height = st.slider('Select graph height', 300, 1500, 900)
 
-with col2:
-    if uploaded_file is not None:
-        # 피벗 테이블 생성 함수
-        def generate_pivot(department, forklift_class):
-            filtered_df = df.copy()
-            if department != '전체':
-                filtered_df = filtered_df[filtered_df['부서'] == department]
-            if forklift_class != '전체':
-                filtered_df = filtered_df[filtered_df['차대 분류'] == forklift_class]
+# 메인 페이지 설정
+if uploaded_file is not None and 'df' in locals():
+    # 피벗 테이블 생성 함수
+    def generate_pivot(department, forklift_class):
+        filtered_df = df.copy()
+        if department != '전체':
+            filtered_df = filtered_df[filtered_df['부서'] == department]
+        if forklift_class != '전체':
+            filtered_df = filtered_df[filtered_df['차대 분류'] == forklift_class]
 
-            pivot_table = filtered_df.pivot_table(index=index_name, columns='시간대', values=value_name, aggfunc=agg_func).fillna(0)
-            return pivot_table
+        if analysis_type == '운영 대수':
+            index_name = '시작 날짜'
+            value_name = '차대 코드'
+            agg_func = 'nunique'
+            title = '시작 날짜 및 시간대별 차대 코드 운영 대수 Heatmap'
+        else:
+            index_name = '차대 코드'
+            value_name = '시작 날짜'
+            agg_func = 'count'
+            title = '차대 코드별 시간대 운영 횟수 Heatmap'
 
-        pivot_table = generate_pivot(selected_department, selected_forklift_class)
+        pivot_table = filtered_df.pivot_table(index=index_name, columns='시간대', values=value_name, aggfunc=agg_func).fillna(0)
+        return pivot_table
 
-        # Heatmap 생성
-        fig = make_subplots(rows=1, cols=1)
-        heatmap = go.Heatmap(
-            z=pivot_table.values,
-            x=pivot_table.columns,
-            y=pivot_table.index,
-            colorscale=[[0, 'white'], [1, 'purple']],
-            hoverinfo='text',
-            text=[[f'{int(val)}번' for val in row] for row in pivot_table.values]
-        )
-        fig.add_trace(heatmap)
-        fig.update_layout(
-            title=title,
-            xaxis=dict(title='시간대', fixedrange=True),
-            yaxis=dict(title=index_name),
-            plot_bgcolor='white',
-            paper_bgcolor='white',
-            margin=dict(l=50, r=50, t=100, b=50),
-            width=800,
-            height=graph_height
-        )
+    pivot_table = generate_pivot(selected_department, selected_forklift_class)
 
-        # Streamlit을 통해 플롯 보여주기
-        st.plotly_chart(fig, use_container_width=True)
+    # Heatmap 생성
+    fig = make_subplots(rows=1, cols=1)
+    heatmap = go.Heatmap(
+        z=pivot_table.values,
+        x=pivot_table.columns,
+        y=pivot_table.index,
+        colorscale=[[0, 'white'], [1, 'purple']],
+        hoverinfo='text',
+        text=[[f'{int(val)}번' for val in row] for row in pivot_table.values]
+    )
+    fig.add_trace(heatmap)
+    fig.update_layout(
+        title=title,
+        xaxis=dict(title='시간대', fixedrange=True),
+        yaxis=dict(title=index_name),
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+        margin=dict(l=50, r=50, t=100, b=50),
+        width=900,  # 고정된 너비
+        height=graph_height  # 조정 가능한 높이
+    )
+
+    # Streamlit을 통해 플롯 보여주기
+    st.plotly_chart(fig, use_container_width=True)
