94555cf1b43700f8a7421f19a7324b9d07ad98a2
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-07-11 17:51:31 +0900

    Update forklift(pw).py

--- forklift(pw).py
+++ forklift(pw).py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objs as go
-from plotly.subplots import make_subplot
+from plotly.subplots import make_subplots
 from openpyxl import load_workbook
 import requests
 import io
@@ -25,39 +25,27 @@ if password:
         columns = next(data)[0:]
         df = pd.DataFrame(data, columns=columns)
 
-        st.write("파일 로드 성공!", df.head())  # 파일이 성공적으로 로드됐다는 메시지와 함께 데이터의 첫 부분을 보여줍니다.
+        st.write("파일 로드 성공!", df.head())
     except Exception as e:
         st.error(f"파일 로드 중 에러 발생: {e}")
 
-# Streamlit 페이지 설정
-st.set_page_config(page_title='My Streamlit App', layout='wide', initial_sidebar_state='expanded')
+# 파일 업로드 및 데이터 처리
+uploaded_file = st.sidebar.file_uploader("파일을 업로드하세요.", type=["csv"])
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    df['시간대'] = pd.to_datetime(df['시간대'], format='%H:%M').dt.strftime('%H:%M')
+    df['시작 날짜'] = pd.to_datetime(df['시작 날짜'])
+    df = df[df['시작 날짜'].dt.month == 5]  # 5월 데이터만 선택
+    df['시작 날짜'] = df['시작 날짜'].dt.strftime('%d')  # 일자 형식으로 변경
+    df = df.sort_values(by=['시작 날짜', '시간대'])
+    df.dropna(subset=['부서', '차대 분류'], inplace=True)
 
-# Streamlit 사이드바 설정
-with st.sidebar:
-    uploaded_file = st.file_uploader("파일을 업로드하세요.", type=["csv"])
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        df['시간대'] = pd.to_datetime(df['시간대'], format='%H:%M').dt.strftime('%H:%M')
+    analysis_type = st.sidebar.radio("분석 유형 선택:", ('운영 대수', '운영 횟수'))
+    selected_department = st.sidebar.selectbox('부서 선택:', ['전체'] + df['부서'].dropna().unique().tolist())
+    selected_forklift_class = st.sidebar.selectbox('차대 분류 선택:', ['전체'] + df['차대 분류'].dropna().unique().tolist())
+    graph_height = st.sidebar.slider('Select graph height', 300, 1500, 900)
 
-        # 날짜를 일자로 파싱하고 특정 월(예: 5월) 데이터만 필터링
-        df['시작 날짜'] = pd.to_datetime(df['시작 날짜'])
-        df = df[df['시작 날짜'].dt.month == 5]  # 5월 데이터만 선택
-        df['시작 날짜'] = df['시작 날짜'].dt.strftime('%d')  # 일자 형식으로 변경
-
-        df = df.sort_values(by=['시작 날짜', '시간대'])
-        df.dropna(subset=['부서', '차대 분류'], inplace=True)
-
-        analysis_type = st.radio("분석 유형 선택:", ('운영 대수', '운영 횟수'))
-        selected_department = st.selectbox('부서 선택:', ['전체'] + df['부서'].dropna().unique().tolist())
-        selected_forklift_class = st.selectbox('차대 분류 선택:', ['전체'] + df['차대 분류'].dropna().unique().tolist())
-        graph_height = st.slider('Select graph height', 300, 1500, 900)
-
-# 변수 초기화
-title = "분석 대기 중..."
-index_name = "데이터 선택"
-
-# 메인 페이지 설정
-if uploaded_file is not None and 'df' in locals():
+    # 피벗 테이블 생성 및 시각화
     def generate_pivot(department, forklift_class):
         filtered_df = df.copy()
         if department != '전체':
@@ -77,9 +65,9 @@ if uploaded_file is not None and 'df' in locals():
             title = '지게차 시간대별 운영 횟수'
 
         pivot_table = filtered_df.pivot_table(index=index_name, columns='시간대', values=value_name, aggfunc=agg_func).fillna(0)
-        return pivot_table, title, index_name
+        return pivot_table, title
 
-    pivot_table, title, index_name = generate_pivot(selected_department, selected_forklift_class)
+    pivot_table, title = generate_pivot(selected_department, selected_forklift_class)
 
     # Heatmap 생성
     fig = make_subplots(rows=1, cols=1)
@@ -89,20 +77,18 @@ if uploaded_file is not None and 'df' in locals():
         y=pivot_table.index,
         colorscale=[[0, 'white'], [1, 'purple']],
         hoverinfo='text',
-        text=[[f' {analysis_type} {int(val)}번' for val in row] for row in pivot_table.values]
+        text=[[f'{analysis_type} {int(val)}번' for val in row] for row in pivot_table.values]
     )
     fig.add_trace(heatmap)
     fig.update_layout(
         title=title,
         xaxis=dict(title='시간대', fixedrange=True),
-        yaxis=dict(title=index_name),
+        yaxis=dict(title=index_name, fixedrange=True),
         plot_bgcolor='white',
         paper_bgcolor='white',
         margin=dict(l=50, r=50, t=100, b=50),
-        width=900,  # 고정된 너비
-        height=graph_height  # 조정 가능한 높이
+        width=900,
+        height=graph_height
     )
 
-    # Streamlit을 통해 플롯 보여주기
     st.plotly_chart(fig, use_container_width=True)
-
