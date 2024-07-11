19c69c16d6d2a53ffbe9287fd17a2d1f5cc8e7a5
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-07-11 12:10:38 +0900

    Update forklift(merge).py

--- forklift(merge).py
+++ forklift(merge).py
@@ -8,45 +8,30 @@ st.set_page_config(page_title='My Streamlit App', layout='wide', initial_sidebar
 
 # Streamlit 사이드바 설정
 with st.sidebar:
-    # Streamlit 파일 업로더를 사용하여 파일 업로드
     uploaded_file = st.file_uploader("파일을 업로드하세요.", type=["csv"])
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
-
-        # 데이터 전처리
         df['시간대'] = pd.to_datetime(df['시간대'], format='%H:%M').dt.strftime('%H:%M')
         df = df.sort_values(by=['시간대'])
         df.dropna(subset=['부서', '차대 분류'], inplace=True)
 
-        # 필터링 및 전처리 옵션
-        analysis_type = st.radio(
-            "분석 유형 선택:",
-            ('운영 대수', '운영 횟수')
-        )
-
-        # 선택 상자를 통한 부서와 차대 분류 선택
-        selected_department = st.selectbox(
-            '부서 선택:',
-            ['전체'] + df['부서'].dropna().unique().tolist()
-        )
-        selected_forklift_class = st.selectbox(
-            '차대 분류 선택:',
-            ['전체'] + df['차대 분류'].dropna().unique().tolist()
-        )
-
-        # 그래프 높이 조정 슬라이더
+        analysis_type = st.radio("분석 유형 선택:", ('운영 대수', '운영 횟수'))
+        selected_department = st.selectbox('부서 선택:', ['전체'] + df['부서'].dropna().unique().tolist())
+        selected_forklift_class = st.selectbox('차대 분류 선택:', ['전체'] + df['차대 분류'].dropna().unique().tolist())
         graph_height = st.slider('Select graph height', 300, 1500, 900)
+
+# 초기화
+title = "분석 대기 중..."  # 초기 제목 설정
 
 # 메인 페이지 설정
 if uploaded_file is not None and 'df' in locals():
-    # 피벗 테이블 생성 함수
     def generate_pivot(department, forklift_class):
         filtered_df = df.copy()
         if department != '전체':
             filtered_df = filtered_df[filtered_df['부서'] == department]
         if forklift_class != '전체':
             filtered_df = filtered_df[filtered_df['차대 분류'] == forklift_class]
-
+        
         if analysis_type == '운영 대수':
             index_name = '시작 날짜'
             value_name = '차대 코드'
@@ -62,7 +47,7 @@ if uploaded_file is not None and 'df' in locals():
         return pivot_table
 
     pivot_table = generate_pivot(selected_department, selected_forklift_class)
-
+    
     # Heatmap 생성
     fig = make_subplots(rows=1, cols=1)
     heatmap = go.Heatmap(
