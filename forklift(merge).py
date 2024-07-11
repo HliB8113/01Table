4ceab4444632410238c02b9a2519bc2577d6d7bb
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-07-11 13:36:17 +0900

    Update forklift(merge).py

--- forklift(merge).py
+++ forklift(merge).py
@@ -12,13 +12,17 @@ with st.sidebar:
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
         df['시간대'] = pd.to_datetime(df['시간대'], format='%H:%M').dt.strftime('%H:%M')
-        df = df.sort_values(by=['시간대'])
+        df['시작 날짜'] = pd.to_datetime(df['시작 날짜'])
+        df = df[df['시작 날짜'].dt.month == 5]  # 5월 데이터만 선택
+        df['시작 날짜'] = df['시작 날짜'].dt.strftime('%d')  # 일자만 표시
+        df = df.sort_values(by=['시작 날짜', '시간대'])
         df.dropna(subset=['부서', '차대 분류'], inplace=True)
 
         analysis_type = st.radio("분석 유형 선택:", ('운영 대수', '운영 횟수'))
         selected_department = st.selectbox('부서 선택:', ['전체'] + df['부서'].dropna().unique().tolist())
         selected_forklift_class = st.selectbox('차대 분류 선택:', ['전체'] + df['차대 분류'].dropna().unique().tolist())
         graph_height = st.slider('Select graph height', 300, 1500, 900)
+        selected_date_range = st.slider("Select date range", int(df['시작 날짜'].min()), int(df['시작 날짜'].max()), (int(df['시작 날짜'].min()), int(df['시작 날짜'].max())))
 
 # 변수 초기화
 title = "분석 대기 중..."
@@ -26,8 +30,9 @@ index_name = "데이터 선택"
 
 # 메인 페이지 설정
 if uploaded_file is not None and 'df' in locals():
-    def generate_pivot(department, forklift_class):
+    def generate_pivot(department, forklift_class, date_range):
         filtered_df = df.copy()
+        filtered_df = filtered_df[(filtered_df['시작 날짜'].astype(int) >= date_range[0]) & (filtered_df['시작 날짜'].astype(int) <= date_range[1])]
         if department != '전체':
             filtered_df = filtered_df[filtered_df['부서'] == department]
         if forklift_class != '전체':
@@ -49,7 +54,7 @@ if uploaded_file is not None and 'df' in locals():
         pivot_table = filtered_df.pivot_table(index=index_name, columns='시간대', values=value_name, aggfunc=agg_func).fillna(0)
         return pivot_table, title, index_name, unit
 
-    pivot_table, title, index_name, unit = generate_pivot(selected_department, selected_forklift_class)
+    pivot_table, title, index_name, unit = generate_pivot(selected_department, selected_forklift_class, selected_date_range)
 
     # Heatmap 생성
     fig = make_subplots(rows=1, cols=1)
@@ -59,7 +64,7 @@ if uploaded_file is not None and 'df' in locals():
         y=pivot_table.index,
         colorscale=[[0, 'white'], [1, 'purple']],
         hoverinfo='text',
-        text=[[f' {analysis_type} {int(val)}{unit}' for val in row] for row in pivot_table.values]
+        text=[[f'운영 {analysis_type} {int(val)}{unit}' for val in row] for row in pivot_table.values]
     )
     fig.add_trace(heatmap)
     fig.update_layout(
@@ -69,8 +74,8 @@ if uploaded_file is not None and 'df' in locals():
         plot_bgcolor='white',
         paper_bgcolor='white',
         margin=dict(l=50, r=50, t=100, b=50),
-        width=900,  # 고정된 너비
-        height=graph_height  # 조정 가능한 높이
+        width=900,
+        height=graph_height
     )
 
     # Streamlit을 통해 플롯 보여주기
