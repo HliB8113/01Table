5531948c0145a0c85d545bd55f104203604dbd25
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-07-17 14:01:39 +0900

    Update forklift(merge).py

--- forklift(merge).py
+++ forklift(merge).py
@@ -18,61 +18,53 @@ with st.sidebar:
         # 시간대를 시간 형식으로 변환
         df['시간대'] = pd.to_datetime(df['시간대'], format='%H:%M', errors='coerce').dt.strftime('%H:%M')
 
-        # 시작 날짜를 날짜 형식으로 변환
+        # 시작 날짜를 날짜 형식으로 변환 및 '월' 열 생성
         df['시작 날짜'] = pd.to_datetime(df['시작 날짜'])
+        df['월'] = df['시작 날짜'].dt.strftime('%m')
 
-        # 월 열 추가
-        df['월'] = df['시작 날짜'].dt.month
-
-        # 5월, 6월, 7월 데이터만 필터링
-        df = df[df['시작 날짜'].dt.month.isin([5, 6, 7])]
-        df['시작 날짜'] = df['시작 날짜'].dt.strftime('%Y-%m-%d')  # 날짜 형식 변경
-        df = df.sort_values(by=['시작 날짜', '시간대'])
-        df.dropna(subset=['부서', '차대 분류'], inplace=True)
-
-        # 공정 드롭다운 추가
-        selected_process = st.selectbox('공정 선택:', ['전체'] + df['공정'].dropna().unique().tolist())
-
-        # 월 드롭다운 추가
+        # 사이드바 '월' 선택 드롭다운 추가
         selected_month = st.selectbox('월 선택:', ['전체'] + sorted(df['월'].unique().tolist()))
+
+        # 필터링 로직 업데이트
+        if selected_month != '전체':
+            df = df[df['월'] == selected_month]
+
+        # 공정 선택 드롭다운 추가
+        selected_process = st.selectbox('공정 선택:', ['전체'] + df['공정'].dropna().unique().tolist())
+        if selected_process != '전체':
+            df = df[df['공정'] == selected_process]
 
         analysis_type = st.radio("분석 유형 선택:", ('운영 대수', '운영 횟수'))
         selected_department = st.selectbox('부서 선택:', ['전체'] + df['부서'].dropna().unique().tolist())
         selected_forklift_class = st.selectbox('차대 분류 선택:', ['전체'] + df['차대 분류'].dropna().unique().tolist())
         graph_height = st.slider('Select graph height', 300, 1500, 900)
 
-# 변수 초기화
-title = "분석 대기 중..."
-index_name = "데이터 선택"
-
-# 메인 페이지 설정
+# 변수 초기화 및 메인 페이지 설정
 if uploaded_file is not None and 'df' in locals():
-    def generate_pivot(department, forklift_class, process, month):
+    def generate_pivot(department, forklift_class):
         filtered_df = df.copy()
         if department != '전체':
             filtered_df = filtered_df[filtered_df['부서'] == department]
         if forklift_class != '전체':
             filtered_df = filtered_df[filtered_df['차대 분류'] == forklift_class]
-        if process != '전체':
-            filtered_df = filtered_df[filtered_df['공정'] == process]
-        if month != '전체':
-            filtered_df = filtered_df[filtered_df['월'] == month]
 
         if analysis_type == '운영 대수':
             index_name = '시작 날짜'
             value_name = '차대 코드'
             agg_func = 'nunique'
             title = '지게차 일자별 운영 대수'
+            pivot_table = filtered_df.pivot_table(index=index_name, columns='시간대', values=value_name, aggfunc=agg_func).fillna(0)
+            pivot_table.index = pivot_table.index.strftime('%m-%d')
         else:
             index_name = '차대 코드'
             value_name = '시작 날짜'
             agg_func = 'count'
             title = '지게차 시간대별 운영 횟수'
+            pivot_table = filtered_df.pivot_table(index=index_name, columns='시간대', values=value_name, aggfunc=agg_func).fillna(0)
 
-        pivot_table = filtered_df.pivot_table(index=index_name, columns='시간대', values=value_name, aggfunc=agg_func).fillna(0)
         return pivot_table, title, index_name
 
-    pivot_table, title, index_name = generate_pivot(selected_department, selected_forklift_class, selected_process, selected_month)
+    pivot_table, title, index_name = generate_pivot(selected_department, selected_forklift_class)
 
     # Heatmap 생성
     fig = make_subplots(rows=1, cols=1)
@@ -82,7 +74,7 @@ if uploaded_file is not None and 'df' in locals():
         y=pivot_table.index,
         colorscale=[[0, 'white'], [1, 'purple']],
         hoverinfo='text',
-        text=[[f' {analysis_type} {int(val)}번' for val in row] for row in pivot_table.values]
+        text=[[f'{analysis_type} {int(val)}번' for val in row] for row in pivot_table.values]
     )
     fig.add_trace(heatmap)
     fig.update_layout(
