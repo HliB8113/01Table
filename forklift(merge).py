a1f67b212613b9811464f9d64010c6aa6542d8f9
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-09-24 14:41:21 +0900

    Update forklift(merge).py

--- forklift(merge).py
+++ forklift(merge).py
@@ -13,17 +13,19 @@ with st.sidebar:
         df = pd.read_csv(uploaded_file)
         
         # 시간대를 시간 형식으로 변환
-        try:
-            df['시간대'] = pd.to_datetime(df['시간대'], format='%H:%M', errors='coerce').dt.strftime('%H:%M')
-        except ValueError:
-            df['시간대'] = pd.to_datetime(df['시간대'], format='%H:%M:%S', errors='coerce').dt.strftime('%H:%M')
-
+        df['시간대'] = pd.to_datetime(df['시간대'], format='%H:%M', errors='coerce').dt.strftime('%H:%M')
+        
         # 시작 날짜를 날짜 형식으로 변환 및 월 열 추가
         df['시작 날짜'] = pd.to_datetime(df['시작 날짜'])
         df['월'] = df['시작 날짜'].dt.month
 
         # 12월 데이터 제외
         df = df[df['월'] != 12]
+
+        # 고유 차대 코드가 부서 데이터와 1대2 관계인 경우를 처리
+        # 부서에 대한 데이터가 중복되어 있는 차대 코드는 하나로 취급
+        df['차대 코드'] = df['차대 코드'].astype(str) + '_' + df['부서'].astype(str)
+        df['차대 코드'] = df['차대 코드'].apply(lambda x: x.split('_')[0] if df[df['차대 코드'] == x]['부서'].nunique() == 1 else x)
 
         # 드롭다운 메뉴 설정
         analysis_type = st.radio("분석 유형 선택:", ('운영 대수', '운영 횟수'))
@@ -59,164 +61,32 @@ if uploaded_file is not None and 'df' in locals():
             value_name = '차대 코드'
             agg_func = 'nunique'
             title = '지게차 일자별 운영 대수'
-            
-            # 월 전체 운영 대수 계산
-            total_operating_units = filtered_df[value_name].nunique()
-            
-            # 월 최소 및 최대 운영 대수 계산
-            daily_counts = filtered_df.groupby('시작 날짜')[value_name].nunique()
-            min_operating_units = daily_counts.min()
-            max_operating_units = daily_counts.max()
-            min_operating_day = daily_counts.idxmin()
-            max_operating_day = daily_counts.idxmax()
 
-            # 비율 계산
-            min_operating_units_ratio = (min_operating_units / total_operating_units) * 100
-            max_operating_units_ratio = (max_operating_units / total_operating_units) * 100
-            
-            summary = {
-                'total_units': total_operating_units,
-                'min_units': min_operating_units,
-                'min_units_day': min_operating_day,
-                'min_units_ratio': min_operating_units_ratio,
-                'max_units': max_operating_units,
-                'max_units_day': max_operating_day,
-                'max_units_ratio': max_operating_units_ratio,
-            }
-        else:
-            index_name = '차대 코드'
-            value_name = '시작 날짜'
-            agg_func = 'count'
-            title = '지게차 시간대별 운영 횟수'
-            
-            # 월 최소 및 최대 운영 횟수 계산
-            unit_counts = filtered_df.groupby(['차대 코드'])['시작 날짜'].count()
-            min_operating_counts = unit_counts.min()
-            max_operating_counts = unit_counts.max()
-            min_operating_unit = unit_counts.idxmin()
-            max_operating_unit = unit_counts.idxmax()
+            pivot_table = filtered_df.pivot_table(index=index_name, columns='시간대', values=value_name, aggfunc=agg_func).fillna(0)
+            return pivot_table, title, index_name
 
-            # 전체 운영 횟수 계산
-            total_operating_counts = unit_counts.sum()
-            
-            # 비율 계산
-            min_operating_counts_ratio = (min_operating_counts / total_operating_counts) * 100
-            max_operating_counts_ratio = (max_operating_counts / total_operating_counts) * 100
-            
-            # 운영 시간 계산
-            filtered_df['운영 시간(초)'] = filtered_df['운영 시간(초)'].astype(int)
-            operating_times = filtered_df.groupby('차대 코드')['운영 시간(초)'].sum()
-            min_operating_time = operating_times.min()
-            max_operating_time = operating_times.max()
-            min_time_unit = operating_times.idxmin()
-            max_time_unit = operating_times.idxmax()
-            
-            # 전체 운영 시간 계산
-            total_operating_time = operating_times.sum()
-
-            # 비율 계산
-            min_operating_time_ratio = (min_operating_time / total_operating_time) * 100
-            max_operating_time_ratio = (max_operating_time / total_operating_time) * 100
-            
-            def format_time(seconds):
-                hours, seconds = divmod(seconds, 3600)
-                minutes, seconds = divmod(seconds, 60)
-                return f"{hours:02}:{minutes:02}:{seconds:02}"
-
-            min_operating_time_formatted = format_time(min_operating_time)
-            max_operating_time_formatted = format_time(max_operating_time)
-            total_operating_time_formatted = format_time(total_operating_time)
-            
-            summary = {
-                'total_counts': total_operating_counts,
-                'min_counts': min_operating_counts,
-                'min_counts_unit': min_operating_unit,
-                'min_counts_ratio': min_operating_counts_ratio,
-                'max_counts': max_operating_counts,
-                'max_counts_unit': max_operating_unit,
-                'max_counts_ratio': max_operating_counts_ratio,
-                'total_time': total_operating_time_formatted,
-                'min_time': min_operating_time_formatted,
-                'min_time_unit': min_time_unit,
-                'min_time_ratio': min_operating_time_ratio,
-                'max_time': max_operating_time_formatted,
-                'max_time_unit': max_time_unit,
-                'max_time_ratio': max_operating_time_ratio,
-            }
-        
-        pivot_table = filtered_df.pivot_table(index=index_name, columns='시간대', values=value_name, aggfunc=agg_func).fillna(0)
-        return pivot_table, title, index_name, summary
-
-    pivot_table, title, index_name, summary = generate_pivot(selected_month, selected_department, selected_process, selected_forklift_class, selected_workplace)
+    pivot_table, title, index_name = generate_pivot(selected_month, selected_department, selected_process, selected_forklift_class, selected_workplace)
 
     # Heatmap 생성
     fig = make_subplots(rows=1, cols=1)
-    tooltip_texts = [[f'{analysis_type} {int(val)}{"대" if analysis_type == "운영 대수" else "번"}' for val in row] for row in pivot_table.values]
+    tooltip_texts = [[f'운영 대수 {int(val)}대' for val in row] for row in pivot_table.values]
     heatmap = go.Heatmap(
         z=pivot_table.values,
         x=pivot_table.columns,
         y=pivot_table.index,
-        colorscale=[[0, 'white'], [1, 'purple']],
+        colorscale='Viridis',
         hoverinfo='text',
-        text=tooltip_texts,
-        zmin=0,
-        zmax=pivot_table.values.max()
+        text=tooltip_texts
     )
     fig.add_trace(heatmap)
     fig.update_layout(
-        title={
-            'text': title,
-            'x': 0.5
-        },
-        xaxis=dict(title='시간대', fixedrange=True),
-        yaxis=dict(title=index_name, categoryorder='array', categoryarray=sorted(pivot_table.index)),
+        title={'text': title, 'x': 0.5},
+        xaxis={'title': '시간대'},
+        yaxis={'title': index_name},
         plot_bgcolor='white',
         paper_bgcolor='white',
-        margin=dict(l=50, r=50, t=150, b=50),
-        width=900,  # 고정된 너비
-        height=graph_height  # 조정 가능한 높이
-    )
-    
-    # 모든 '시작 날짜'를 세로축에 표시 (월일만 표시)
-    if analysis_type == '운영 대수':
-        fig.update_yaxes(type='category', tickmode='array', tickvals=sorted(pivot_table.index))
-
-    # 요약 정보를 가로로 배치하여 표시
-    if analysis_type == '운영 대수':
-        summary_text = (
-            f"<b>운영 대수</b><br>"
-            f"전체: {summary.get('total_units', 'N/A')}대<br>"
-            f"최소: {summary.get('min_units_day', 'N/A')} {summary.get('min_units', 'N/A')}대 ({summary.get('min_units_ratio', 0):.2f}%)<br>"
-            f"최대: {summary.get('max_units_day', 'N/A')} {summary.get('max_units', 'N/A')}대 ({summary.get('max_units_ratio', 0):.2f}%)<br>"
-        )
-    else:
-        summary_text = (
-            f"<b>운영 횟수</b><br>"
-            f"전체: {summary.get('total_counts', 'N/A')}번<br>"
-            f"최소: {summary.get('min_counts_unit', 'N/A')} {summary.get('min_counts', 'N/A')}번 ({summary.get('min_counts_ratio', 0):.2f}%)<br>"
-            f"최대: {summary.get('max_counts_unit', 'N/A')} {summary.get('max_counts', 'N/A')}번 ({summary.get('max_counts_ratio', 0):.2f}%)<br>"
-            f"<br><b>운영 시간</b><br>"
-            f"전체: {summary.get('total_time', 'N/A')}<br>"
-            f"최소: {summary.get('min_time_unit', 'N/A')} {summary.get('min_time', 'N/A')} ({summary.get('min_time_ratio', 0):.2f}%)<br>"
-            f"최대: {summary.get('max_time_unit', 'N/A')} {summary.get('max_time', 'N/A')} ({summary.get('max_time_ratio', 0):.2f}%)"
-        )
-    
-    # 요약 정보 위치 조정 (그래프 높이에 따라)
-    annotation_y = 1.015 + (150 / graph_height)
-
-    fig.add_annotation(
-        text=summary_text,
-        align='left',
-        showarrow=False,
-        xref='paper',
-        yref='paper',
-        x=0,
-        y=annotation_y,
-        bordercolor='black',
-        borderwidth=1,
-        bgcolor='white',
-        opacity=0.8,
-        font=dict(color='black', size=12)  # 텍스트 색상을 검은색으로 지정, 폰트 크기 조정
+        margin={'l': 40, 'r': 40, 't': 80, 'b': 40},
+        height=graph_height
     )
 
     # Streamlit을 통해 플롯 보여주기
