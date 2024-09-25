3abead1a1d14d124440807e7ead5aabae9d0e679
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-09-25 18:52:37 +0900

    Update forklift(merge).py

--- forklift(merge).py
+++ forklift(merge).py
@@ -201,7 +201,7 @@ if uploaded_file is not None and 'df' in locals():
             f"전체: {summary.get('total_units', 'N/A')}대<br>"
             f"최소: {summary.get('min_units_day', 'N/A')} {summary.get('min_units', 'N/A')}대 ({float(summary.get('min_units_ratio', 0)):0.2f}%)<br>"
             f"최대: {summary.get('max_units_day', 'N/A')} {summary.get('max_units', 'N/A')}대 ({float(summary.get('max_units_ratio', 0)):0.2f}%)<br>"
-            f"평균: {float(summary.get('avg_units', 0)):0.2f}대 ({int(summary.get('avg_units_ratio', 0)):0.2f}%)<br>"
+            f"평균: {summary.get('avg_units', 0)):0.2f}대 ({int(summary.get('avg_units_ratio', 0)):0.2f}%)<br>"
         )
     else:
         summary_text = (
