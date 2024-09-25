7627f05b838fb4b529d6278ebed99f920e4ad361
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-09-25 16:23:34 +0900

    Update forklift(merge).py

--- forklift(merge).py
+++ forklift(merge).py
@@ -115,7 +115,7 @@ if uploaded_file is not None and 'df' in locals():
             max_operating_time = operating_times.max()
             min_time_unit = operating_times.idxmin()
             max_time_unit = operating_times.idxmax()
-            avg_operating_time = round(operating_times.mean(), -1)
+            avg_operating_time = roundup(operating_times.mean(), 1)
             
             # 전체 운영 시간 계산
             total_operating_time = operating_times.sum()
