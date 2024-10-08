776daa747f4419705f02fc90e04354e0c64a3df6
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-10-08 15:09:54 +0900

    Update forklift(merge).py

--- forklift(merge).py
+++ forklift(merge).py
@@ -226,7 +226,8 @@ if uploaded_file is not None and 'df' in locals():
         )
     else:
         summary_text = (
-            f"<b>운영 횟수</b>f"전체: {summary.get('total_counts', 'N/A')}번<br>"
+            f"<b>운영 횟수</b><br>"
+            f"전체: {summary.get('total_counts', 'N/A')}번<br>"
             f"일일 최소 운영: {summary.get('min_counts_unit', 'N/A')} {summary.get('min_counts', 'N/A')}번 ({float(summary.get('min_counts_ratio', 0)):0.2f}%)<br>"
             f"일일 최대 운영: {summary.get('max_counts_unit', 'N/A')} {summary.get('max_counts', 'N/A')}번 ({float(summary.get('max_counts_ratio', 0)):0.2f}%)<br>"
             f"일일 평균 운영: {summary.get('avg_counts', 'N/A')}번 ({float(summary.get('avg_counts_ratio', 0)):0.2f}%)<br>"
