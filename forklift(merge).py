6b2f08721c0456e26075b04cb2febfed8a3fd834
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-10-08 15:17:22 +0900

    Update forklift(merge).py

--- forklift(merge).py
+++ forklift(merge).py
@@ -218,7 +218,7 @@ if uploaded_file is not None and 'df' in locals():
     if analysis_type == '운영 대수':
         summary_text = (
             f"<b>운영 대수</b> ("
-            f"전체: {summary.get('total_units', 'N/A')}대<br>"
+            f"전체: {summary.get('total_units', 'N/A')}대)<br>"
             f"일일 최소 운영: {summary.get('min_units_day', 'N/A')} {summary.get('min_units', 'N/A')}대 ({float(summary.get('min_units_ratio', 0)):0.2f}%)<br>"
             f"일일 최대 운영: {summary.get('max_units_day', 'N/A')} {summary.get('max_units', 'N/A')}대 ({float(summary.get('max_units_ratio', 0)):0.2f}%)<br>"
             f"일일 평균 운영: {summary.get('avg_units', 'N/A')}대 ({float(summary.get('avg_units_ratio', 0)):0.2f}%)<br>"
@@ -227,12 +227,12 @@ if uploaded_file is not None and 'df' in locals():
     else:
        summary_text = (
             f"<b>운영 횟수</b> ("
-            f"전체: {summary.get('total_counts', 'N/A')}번<br>"
+            f"전체: {summary.get('total_counts', 'N/A')}번)<br>"
             f"일일 최소 운영: {summary.get('min_counts_unit', 'N/A')} {summary.get('min_counts', 'N/A')}번 ({float(summary.get('min_counts_ratio', 0)):0.2f}%)<br>"
             f"일일 최대 운영: {summary.get('max_counts_unit', 'N/A')} {summary.get('max_counts', 'N/A')}번 ({float(summary.get('max_counts_ratio', 0)):0.2f}%)<br>"
             f"일일 평균 운영: {summary.get('avg_counts', 'N/A')}번 ({float(summary.get('avg_counts_ratio', 0)):0.2f}%)<br>"
             f"<b>운영 시간</b>"
-            f"전체: {summary.get('total_time', 'N/A')}<br>"
+            f"전체: {summary.get('total_time', 'N/A')})<br>"
             f"일일 최소 운영: {summary.get('min_time_unit', 'N/A')} {summary.get('min_time', 'N/A')} ({float(summary.get('min_time_ratio', 0)):0.2f}%)<br>"
             f"일일 최대 운영: {summary.get('max_time_unit', 'N/A')} {summary.get('max_time', 'N/A')} ({float(summary.get('max_time_ratio', 0)):0.2f}%)<br>"
             f"일일 평균 운영: {summary.get('avg_time', 'N/A')} ({float(summary.get('avg_time_ratio', 0)):0.2f}%)<br>"
