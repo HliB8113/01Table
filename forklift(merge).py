295943040f1a78ab99da8d2ed14697bbdf6c464b
HliB8113 <102398569+HliB8113@users.noreply.github.com>
2024-10-11 10:43:31 +0900

    Update forklift(merge).py

--- forklift(merge).py
+++ forklift(merge).py
@@ -199,7 +199,7 @@ if uploaded_file is not None and 'df' in locals():
         title={
             'text': title,
             'x': 0.5,
-            'font': {'size': 20}  # 제목 크기 설정
+            'font': {'size': 25}  # 제목 크기 설정
         },
         xaxis=dict(title='시간대', fixedrange=True),
         yaxis=dict(title=index_name, categoryorder='array', categoryarray=sorted(pivot_table.index)),
@@ -218,18 +218,18 @@ if uploaded_file is not None and 'df' in locals():
     # 요약 정보를 가로로 배치하여 표시
     if analysis_type == '운영 대수':
         summary_text = (
-            f"<b>운영 대수 전체: {summary.get('total_units', 'N/A')}대</b><br>"
+            f"<b>운영 대수(전체: {summary.get('total_units', 'N/A')}대)</b><br>"
             f"일일 최소 운영: {summary.get('min_units_day', 'N/A')} {summary.get('min_units', 'N/A')}대 ({float(summary.get('min_units_ratio', 0)):0.2f}%)<br>"
             f"일일 최대 운영: {summary.get('max_units_day', 'N/A')} {summary.get('max_units', 'N/A')}대 ({float(summary.get('max_units_ratio', 0)):0.2f}%)<br>"
             f"일일 평균 운영: {summary.get('avg_units', 'N/A')}대 ({float(summary.get('avg_units_ratio', 0)):0.2f}%)<br>"
         )
     else:
         summary_text = (
-            f"<b>운영 횟수 전체: {summary.get('total_counts', 'N/A')}번</b><br>"
+            f"<b>운영 횟수(전체: {summary.get('total_counts', 'N/A')}번)</b><br>"
             f"일일 최소 운영: {summary.get('min_counts_unit', 'N/A')} {summary.get('min_counts', 'N/A')}번 ({float(summary.get('min_counts_ratio', 0)):0.2f}%)<br>"
             f"일일 최대 운영: {summary.get('max_counts_unit', 'N/A')} {summary.get('max_counts', 'N/A')}번 ({float(summary.get('max_counts_ratio', 0)):0.2f}%)<br>"
             f"일일 평균 운영: {summary.get('avg_counts', 'N/A')}번 ({float(summary.get('avg_counts_ratio', 0)):0.2f}%)<br>"
-            f"<b>운영 시간 전체: {summary.get('total_time', 'N/A')}</b><br>"
+            f"<b>운영 시간(전체: {summary.get('total_time', 'N/A')})</b><br>"
             f"일일 최소 운영: {summary.get('min_time_unit', 'N/A')} {summary.get('min_time', 'N/A')} ({float(summary.get('min_time_ratio', 0)):0.2f}%)<br>"
             f"일일 최대 운영: {summary.get('max_time_unit', 'N/A')} {summary.get('max_time', 'N/A')} ({float(summary.get('max_time_ratio', 0)):0.2f}%)<br>"
             f"일일 평균 운영: {summary.get('avg_time', 'N/A')} ({float(summary.get('avg_time_ratio', 0)):0.2f}%)<br>"
